86feaf726f508acaae4eb75b63a86799ccae98a3
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-04-12 00:08:00 -0500

    Ajustes post, modulos importados

--- src/application.py
+++ src/application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from src.models import db
-from src.views import (ViewBlacklist)
+from models import db
+from views import (ViewBlacklist,ViewBlacklistGet)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:devops1234@database-blacklist.cyxrkg2exb5u.us-east-2.rds.amazonaws.com:5432/blacklist'
@@ -15,5 +15,8 @@ db.init_app(app)
 db.create_all()
 
 api = Api(app)
-api.add_resource(ViewBlacklist, '/blacklists/<string:email>')
+api.add_resource(ViewBlacklistGet, '/blacklists/<string:email>')
 api.add_resource(ViewBlacklist, '/blacklists')
+
+if __name__ == "__main__":
+    app.run(port = 5000, debug = True)
--- src/views/views.py
+++ src/views/views.py
@@ -2,12 +2,12 @@ from flask import request
 from flask_restful import Resource
 from flask_sqlalchemy import Model, SQLAlchemy
 import datetime 
-from src.models import Blacklist, BlacklistSchema
+from models import Blacklist, BlacklistSchema, db
 
 blacklist_schema = BlacklistSchema()
 BEARER_TOKEN = "Bearer kfsdmxcvnms82439wwqeqe1sdfs5sdqe56"
 
-class ViewBlacklist(Resource):
+class ViewBlacklistGet(Resource):
 
     def get(self, email:str):
         if 'Authorization' in request.headers:
@@ -22,6 +22,8 @@ class ViewBlacklist(Resource):
             return ({"is_in_black_list": True}), 200
         else:
             return ({"is_in_black_list": False}), 200
+        
+class ViewBlacklist(Resource):
     
     def post(self,):
         if 'Authorization' in request.headers:
@@ -35,10 +37,11 @@ class ViewBlacklist(Resource):
             if 'email' in request.json and 'app_uuid' in request.json and 'blocked_reason' in request.json:
                 newBlacklist = Blacklist(email = request.json.get("email"),
                                         app_uuid = request.json.get("app_uuid"),
-                                        blockd_reason = request.json.get("blocked_reason"),
+                                        blocked_reason = request.json.get("blocked_reason"),
                                         timestamp = datetime.datetime.utcnow,
                                         client_ip = request.remote_addr)
                 db.session.add(newBlacklist)
+                db.session.commit()
                 return {"Code": "200", "message": "blakclist created"}, 200
             else:
                 return {"status_code": "400", "message": ""}, 400
